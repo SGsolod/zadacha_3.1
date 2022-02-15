2c13799826c6e12661f3044de456f844b0442045
Sergei Solodovnikov <sergeisolodovnikov0@gmail.com>
2022-02-16 01:41:48 +0300

    Теория понятна, нужо больше практики.

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,15 +1,11 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int ticketPrice = 10000;
-        boolean bonus = ticketPrice > 20;
-        if (bonus) {
-            int x = ticketPrice / 20;
-            System.out.println("Вам начислено" + " " + x + " " + "бонусных миль.");
-        } else {
-            System.out.println("Возьмите с собой друзей для начисления бонусных миль.");
-        }
-        System.out.println("Приятного путешествия!");
-        return ;
+         // цена разделить на 20 миль
+        int bonus = cost / 20;
+        System.out.println("Вам зачислено:");
+        return bonus;
+
+
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
-        System.out.println(miles);
+        System.out.println(miles + " бонусных миль!");
+
     }
 }
 
